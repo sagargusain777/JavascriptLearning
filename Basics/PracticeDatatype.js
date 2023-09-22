db97f0e33b0935248a2a130d5fcf3e87a56b62cf
SAGAR SINGH GUSAIN <131802238+uwuxverticuse@users.noreply.github.com>
2023-09-22 11:43:26 +0000

    Practice of DataTypes

---
+++ Basics/PracticeDatatype.js
@@ -0,0 +1,32 @@
+//String Data type
+let name ="Sagar"
+console.log(typeof name)
+console.log(typeof "Simran")
+console.log(typeof ("!qWEERRSssk-="))
+//Number Data type
+let dob =20102002
+let age =22
+console.log(typeof (dob))
+console.log(typeof 22)
+console.log(typeof age)
+//Bigint => Usually used for large number e.g=> Stockmarkets,cryptocurrency prices , Tradying etc
+
+//BooleanDatatype
+let isloggedin = true;
+let areyoumale = false;
+console.log(typeof isloggedin)
+console.log(typeof (areyoumale))
+console.log(typeof true)
+console.log(typeof (false))
+//Undefined
+let str= undefined
+let ligma
+console.log(typeof str)
+console.log(typeof ligma)
+console.log(typeof undefined)
+
+
+
+
+
+
