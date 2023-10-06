0d47e6f2be60e521fa89d275cc60caa581480bfc
SAGAR SINGH GUSAIN <131802238+uwuxverticuse@users.noreply.github.com>
2023-10-06 13:59:47 +0000

    String to Boolean Conversion

--- Basics/DataTypeConversion.js
+++ Basics/DataTypeConversion.js
@@ -18,6 +18,8 @@ console.log(typeof ValueinNumber2)
 // Printing the Value inside ValueinNumber2 Variable
 console.log(ValueinNumber2);
 
+//"33"-->33
+//"33abc"-->NaN
 
 //Special Cases Of Null
 let value = null;
@@ -27,6 +29,8 @@ console.log(typeof valuenumber);
 //Printing the valuenumber
 console.log(valuenumber);
 
+//null-->0
+
 //Special Case of Undefined
 let age = undefined;
 console.log(typeof age);
@@ -35,6 +39,8 @@ let ageNumber =Number(age);
 console.log(typeof (ageNumber));
 //Printing the ageNumber Variable
 console.log(ageNumber);
+
+//undefined-->0
 
 
 
@@ -47,4 +53,41 @@ console.log(typeof (isloggedNumber));
 //printing the New Created Variable
 console.log(isloggedNumber);
 
+// False-->0
+//True-->1
+
+
+//Number To Boolean Conversion
+let adio = 1;
+console.log(typeof adio);
+
+let booladio = Boolean(adio)
+console.log(typeof (booladio));
+console.log(booladio);
+
+
+let nike =0;
+console.log(typeof (nike));
+let nikebool=Boolean(nike);
+console.log(typeof nikebool);
+
+console.log(nikebool);
+
+//1--> True
+//0-->False
+//""-->False
+//"SAGAR"--> True
+
+//  String To Boolean Conversion
+let stoner ="";
+console.log(typeof stoner);
+let stonerbool=Boolean(stoner);
+console.log(typeof stonerbool);
+console.log(stonerbool);
+
+let stoner1="LIGMA";
+console.log(typeof stoner1);
+let stoner1bool=Boolean(stoner1);
+console.log(typeof stoner1bool);
+console.log(stoner1bool);
 
